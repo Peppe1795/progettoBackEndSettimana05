2030336bdaa0a7f58d942e67e0521136183b9edc
Peppe1795 <petrucci238@gmail.com>
2023-08-04 10:12:36 +0200

    start project

---
+++ src/test/java/Giuseppe/gestione_dispositivi/GestioneDispositiviApplicationTests.java
@@ -0,0 +1,13 @@
+package Giuseppe.gestione_dispositivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispositiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
