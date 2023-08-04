972d46479cd828184d9b1dd1befcc521fdb227ba
Peppe1795 <petrucci238@gmail.com>
2023-08-04 16:41:30 +0200

    fine

--- src/main/java/Giuseppe/gestione_dispositivi/dispositivoAssegnato/DispositiviAssegnatiService.java
+++ src/main/java/Giuseppe/gestione_dispositivi/dispositivoAssegnato/DispositiviAssegnatiService.java
@@ -24,30 +24,25 @@ public class DispositiviAssegnatiService {
 	}
 
 	public String assegnaDispositivo(UUID utenteId, UUID dispositivoId) {
-		// Controlla se il dispositivo esiste nel database
 		Dispositivi dispositivo = dispositivoService.findById(dispositivoId);
 		if (dispositivo == null) {
 			return "Dispositivo non trovato.";
 		}
 
-		// Controlla se il dispositivo è in manutenzione
 		if (dispositivo.getStatoDispositivo() == StatoDispositivo.IN_MANUTENZIONE) {
 			return "Il dispositivo è in manutenzione e non può essere assegnato.";
 		}
 
-		// Controlla se il dispositivo è già assegnato all'utente corrente
 		if (dispositivo.getStatoDispositivo() == StatoDispositivo.ASSEGNATO && dispositivo.getUtente() != null
 				&& dispositivo.getUtente().getId().equals(utenteId)) {
 			return "Il dispositivo è già assegnato all'utente.";
 		}
 
-		// Controlla se l'utente esiste nel database
 		Utente utente = utenteService.findById(utenteId);
 		if (utente == null) {
 			return "Utente non trovato.";
 		}
 
-		// Assegna il dispositivo all'utente e cambia il suo stato a "ASSEGNATO"
 		dispositivo.setUtente(utente);
 		dispositivo.setStatoDispositivo(StatoDispositivo.ASSEGNATO);
 		dispositivoService.saveDispositivo(dispositivo);
